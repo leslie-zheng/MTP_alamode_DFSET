35431bd2fe44ff3d0918ad2cf3502635b89855cb
Jiongzhi ZHENG <jzhengap@connect.ust.hk>
2024-11-14 01:38:03 -0500

    Update mtp_to_alamode_dfset.py

--- mtp_to_alamode_dfset.py
+++ mtp_to_alamode_dfset.py
@@ -5,28 +5,18 @@ import xml.etree.ElementTree as ET
 import numpy as np
 import glob
 
-# Script to convert VASP outputs(MTP fake vasprun.xml) to a single DFSET file for ALAMODE
+# Script to convert VASP outputs (MTP fake vasprun.xml) to a single DFSET file for ALAMODE
 # author: Jiongzhi Zheng
 # email: jiongzhi.zheng@dartmouth.edu
 # institution: Dartmouth College
 # date: 2024-11-13
-# version: 1.0
-
-# usage: python3 mtp_to_alamode_dfset.py
-# This script reads the POSCAR file and a series of vasprun_*.xml files in the directory and writes the data to a single DFSET file.
-# The DFSET file contains the Cartesian coordinates of the atoms, the forces on the atoms, 
-# and the displacements of the atoms from the equilibrium positions.
-
-# The displacements are adjusted to be within the -0.5 to 0.5 range.
-# The units are converted from Angstrom to Bohr for positions and from eV/Angstrom to Ryd/bohr for forces.
-
+# version: 1.1
 
 # Define the path to the directory containing the files
 path = "/Users/jiongzhizheng/Desktop/harm_vasp_outputs/"
 
 # Read the POSCAR file and initialize Phonopy object
 unitcell = read_vasp(path + "POSCAR")
-# change the supercell size to your supercell size
 phonon = Phonopy(unitcell, [[2, 0, 0], [0, 2, 0], [0, 0, 2]])
 
 # Get the supercell and its lattice vectors
@@ -45,6 +35,11 @@ def extract_varray_data(root, varray_name):
         data.append(values)
     return data
 
+# Read offset forces from offset.xml
+offset_tree = ET.parse(path + "offset.xml")
+offset_root = offset_tree.getroot()
+offset_forces = np.array(extract_varray_data(offset_root, "forces"))
+
 # Prepare to write the combined data to a single DFSET file
 with open(path + "DFSET", "w") as f:
 
@@ -58,6 +53,9 @@ with open(path + "DFSET", "w") as f:
         positions = np.array(extract_varray_data(root, "positions"))
         forces = np.array(extract_varray_data(root, "forces"))
 
+        # Adjust forces by deducting offset forces
+        adjusted_forces = forces - offset_forces
+
         # Convert positions to fractional coordinates
         positions_frac = np.dot(positions, lattice_inv)
         disps = positions_frac - phonon.supercell.get_scaled_positions()
@@ -69,12 +67,12 @@ with open(path + "DFSET", "w") as f:
         # Convert fractional displacements to Cartesian coordinates
         positions_cart = np.dot(disps, lattice)
 
-        # Convert units: positions from Angstrom to Bohr and forces from eV/Angstrom to Ryd/bohr
+        # Convert units: positions from Angstrom to Bohr and adjusted forces from eV/Angstrom to Ryd/bohr
         positions_cart *= 1.88973  # Angstrom to Bohr
-        forces *= 0.038935     # eV/Angstrom to Ryd/bohr
+        adjusted_forces *= 0.038935  # eV/Angstrom to Ryd/bohr
 
         # Write the data to DFSET with a header line for each file
         f.write(f"# {vasprun_file}\n")
         for i in range(len(positions_cart)):
             f.write(f"{positions_cart[i][0]:.16f} {positions_cart[i][1]:.16f} {positions_cart[i][2]:.16f}     "
-                    f"{forces[i][0]:.16f} {forces[i][1]:.16f} {forces[i][2]:.16f}\n")
+                    f"{adjusted_forces[i][0]:.16f} {adjusted_forces[i][1]:.16f} {adjusted_forces[i][2]:.16f}\n")
